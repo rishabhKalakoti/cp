276c4376656c37864af7d0aef296eeb241abb49a
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2018-07-21 15:31:10 +0530

    update basic template

--- basicTemplate.cpp
+++ basicTemplate.cpp
@@ -1,20 +1,30 @@
+/*----------------------
+Author: Rishabh Kalakoti
+Organisation: MNIT Jaipur
+------------------------*/
+
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ii pair<ll,ll>
 #define vi vector<ll>
 #define ff first
 #define ss second
 #define vii vector<ii>
 #define vvi vector<vi>
-#define MOD 100000007
-#define trace(x) printf("%lli\n",x)
-#define trace2(x,y) printf("%lli %lli",x,y)
+#define MOD 1000000007
+#define trace(x) printf("%lli ",x)
+#define trace2(x,y) printf("%lli %lli\n",x,y)
 #define lf printf("\n")
 #define sc(n) scanf("%lli",&n)
 #define sc2(n,m) scanf("%lli",&n,&m)
 #define REP(i,n) for(ll i=0;i<n;i++)
 #define ll long long int
 #define all(v) (v.begin(),v.end())
+#define mp make_pair
+#define pb push_back
+#define test ll T;sc(T);while(T--)
+
 int main()
 {
     
